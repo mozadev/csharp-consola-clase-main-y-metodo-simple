52cb86f3f9381c42a51db734e9f1c201d2756306
Cesar <30559667+CesarAroon@users.noreply.github.com>
2021-04-02 22:48:10 -0500

    Add files via upload

---
+++ youtube/youtube/Program.cs
@@ -0,0 +1,25 @@
+﻿//bibliotecas
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//proyecto
+namespace youtube
+{
+    //clase
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            metodos m = new metodos();//construyendo objeto
+            m.mensaje();
+
+            Console.ReadKey();
+            //creando objeto
+            //hola que hace
+        }
+    }
+}
---
+++ youtube/youtube/metodos.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace youtube
+{
+    class metodos
+    {
+        //atributos
+        //metodo constructor
+        public metodos()
+        {
+            Console.WriteLine("creando el objeto");//construyendo el constructor
+        }
+        
+      
+        //metodos
+        public void mensaje ()
+        {
+            Console.WriteLine("hola que hace");
+
+        }
+
+
+    }
+}
